1af45630e0c9af0067e0613d88000a570937439f
Christoph Wottawa <christoph.wottawa@me.com>
2019-03-27 01:33:55 +0100

    defined entry points for non-linked binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// entry point on Linux:
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    main();
+}
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
+
+// handle panics
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
