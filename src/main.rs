4f16f1aab754cfdd3e719c4763f6bcf0be54cb04
Christoph Wottawa <christoph.wottawa@me.com>
2019-04-03 14:21:05 +0200

    enabled the use of println! and print! macros on the vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,15 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 // handle panics
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello, World!";
 
 // entry point on Linux:
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    //cast integer to raw pointer to vga buffer address:
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            //set the letter/byte
-            *vga_buffer.offset(i as isize * 2) = byte;
-            //set the color (0xb = light cyan)
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xf;
-        }
-    }
-
-    vga_buffer::print_something();
-
+    println!("Hello World from {}", "C_OS");
+    panic!("The kernel paniced");
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -119,17 +119,30 @@ impl Writer {
     }
 }
 
-pub fn print_something() {
-    use core::fmt::Write;
+use lazy_static::lazy_static;
+use spin::Mutex;
 
-    let mut writer = Writer {
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+    });
+}
 
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!\nTest\n");
-    writeln!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
