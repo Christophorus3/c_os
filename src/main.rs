9594827318557d004dcdbf7f6a134daf5186245f
Christoph Wottawa <christoph.wottawa@me.com>
2019-04-03 17:59:39 +0200

    added unit tests for the vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-#![no_std]
-#![no_main]
+#![cfg_attr(test, allow(unused_imports))]
+#![cfg_attr(not(test), no_std)]
+// don't use no_main for testing
+#![cfg_attr(not(test), no_main)]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 // handle panics
+// don't compile for testing
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
@@ -12,6 +16,8 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 // entry point on Linux:
+// don't compile for testing
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World from {}", "C_OS");
